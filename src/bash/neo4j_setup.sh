6248527fa92652dfd2cff18839328431b2071c02
Stephen <sw5@bcm.edu>
2018-09-16 19:17:27 -0400

    Added script to set up neo4j instance

---
+++ src/bash/neo4j_setup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Create keypair
+export KEY_NAME="Neo4j-AWSMarketplace-Key"
+aws ec2 create-key-pair \
+  --key-name $KEY_NAME \
+  --query 'KeyMaterial' \
+  --output text > $KEY_NAME.pem
+
+# Create security group
+export GROUP="neo4j-sg"
+aws ec2 create-security-group \
+  --group-name $GROUP \
+  --description "Neo4j security group"
+
+# Open ports
+for port in 22 7474 7473 7687; do
+  aws ec2 authorize-security-group-ingress --group-name $GROUP --protocol tcp --port $port --cidr 0.0.0.0/0
+done
+
+# Spin up instance. Ami image for us-east-1
+aws ec2 run-instances \
+  --image-id ami-2a652655 \
+  --count 1 \
+  --instance-type m4.large \
+  --key-name $KEY_NAME \
+  --security-groups $GROUP \
+  --query "Instances[*].InstanceId" \
+  --region us-east-1
