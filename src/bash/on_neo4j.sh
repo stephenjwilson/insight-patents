db994a4bde1f1596721522a042188c7c3322d37e
Ubuntu <ubuntu@ip-172-31-25-79.ec2.internal>
2018-10-05 17:45:12 +0000

    Neo4j changes for large dataset and for eliminating edges that lack a patent in the database

--- src/bash/on_neo4j.sh
+++ src/bash/on_neo4j.sh
@@ -1,21 +1,27 @@
 #!/usr/bin/env bash
 
-cd /home/ubuntu/insight-patents/src/bash/
-mkdir neo4jEdges
+cd /var/lib/neo4j/data
+sudo mkdir neo4jEdges
+sudo chown ubuntu neo4jEdges
 cd neo4jEdges
 aws s3 cp s3://edges-to-neo4j . --recursive
 array=(edges_*/p*)
 
 cat ${array[@]} > combined_edges.csv
-sed -i "1s/.*/:START_ID(Patent),:END_ID(Patent)/" combined_edges.csv
-psql -h test-db2.co89ijjtewjb.us-east-1.rds.amazonaws.com -p 5432 -U swilson patent_data -c "\copy (Select patent_number, title From patents) To '/home/ubuntu/insight-patents/src/bash/neo4jEdges/tmp.csv' With CSV"
+psql -h test-db2.co89ijjtewjb.us-east-1.rds.amazonaws.com -p 5432 -U swilson patent_data -c "\copy (Select patent_number, title From patents) To '/var/lib/neo4j/data/neo4jEdges/tmp.csv' With CSV"
+
+python3 ~/insight-patents/src/python/quick_nodes.py tmp.csv combined_edges.csv #python3 ~/insight-patents/src/python/quick_nodes.py combined_edges.csv nodes.csv
+#awk -F',' '{print $1}' combined_edges.csv | uniq -u > tmpNodes1
+#awk -F',' '{print $2}' combined_edges.csv | uniq -u > tmpNodes2
+#cat tmpNodes2 tmpNodes2 | uniq -u > toaddNodes
+#cat nodes.csv toaddNodes > nodes.csv
+
+sed -i "1s/.*/:START_ID(Patent),:END_ID(Patent)/" trim_edges.csv
 echo "patent_number:ID(Patent),title" > node_header.csv
 cat node_header.csv tmp.csv > nodes.csv
-
-python3 ~/insight-patents/src/python/quick_nodes.py combined_edges.csv nodes.csv
 
 # Configure neo4j?
 
 sudo rm -rf /var/lib/neo4j/data/databases/graph1.db
-sudo /usr/bin/neo4j-import --into /var/lib/neo4j/data/databases/graph1.db --id-type string --nodes:Patent nodes.csv --relationships:CITES combined_edges.csv
+sudo /usr/bin/neo4j-import --into /var/lib/neo4j/data/databases/graph1.db --id-type string --nodes:Patent nodes.csv --relationships:CITES trim_edges.csv
 sudo chown -R neo4j:nogroup /var/lib/neo4j/data/databases/graph1.db
---
+++ src/python/quick_nodes.py
@@ -0,0 +1,30 @@
+import sys
+
+def main(nodes, edges):
+    """ Filters the edges list by nodes that actually exist"""
+    fl=open(nodes).readlines()
+
+    nodes = {}
+    for line in fl[1:]:
+        if line!='\n':
+            line=line.strip().split(',')
+            nodes[line[0]]=1
+    to_keep = open('trim_edges.csv','w')
+    f = open(edges)
+    for line in f:
+        line = line.strip().split(',')
+
+        try:
+            nodes[line[0]]
+        except:
+            continue
+
+        try:
+            nodes[line[1]]
+        except:
+            continue
+        to_keep.write(','.join(line)+'\n')
+    to_keep.close()
+
+if __name__=="__main__":
+    main(sys.argv[1],sys.argv[2])
